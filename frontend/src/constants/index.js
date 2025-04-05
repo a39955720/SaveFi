a3f6f739c7d3789a8d40775c271184516a325058
DDEENNY <a39955720@gmail.com>
2025-04-05 19:50:41 +0800

    Completed plan page

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,9 @@
+const saveFiAbi = require("./SaveFiAbi.json");
+const SAVEFI_ADDRESS = "0x382e3789d5C05341dB15c64b7939a6f636935D43";
+const USDC_ADDRESS = "0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8";
+
+module.exports = {
+  saveFiAbi,
+  SAVEFI_ADDRESS,
+  USDC_ADDRESS,
+};
